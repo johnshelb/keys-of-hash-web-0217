365ba2fdb07b6c34258627d2556bdeb357e72f24
John Shelby <jsnd11@gmail.com>
2017-01-04 11:57:21 -0500

    cute animals

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    keysofinterest=[]
+    for i in (0...values.count)
+      if arguments.include?(values[i])
+      keysofinterest<<keys[i]
+      end
+    end
+    keysofinterest
   end
 end
